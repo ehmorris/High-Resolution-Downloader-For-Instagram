999d7858f8df450373f3d19906c71ecd97805ba4
Edwin Morris <edwin@rapgenius.com>
2017-10-09 22:00:46 -0400

    remove provider and store for content script

--- content/src/scripts/index.js
+++ content/src/scripts/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import {Store} from 'react-chrome-redux';
-
 import App from './components/app/App';
-
-const proxyStore = new Store({portName: 'example'});
 
 const anchor = document.createElement('div');
 anchor.id = 'instagram-high-resolution-download-button';
 
 document.body.insertBefore(anchor, document.body.childNodes[0]);
 
-render(
-  <Provider store={proxyStore}>
-    <App/>
-  </Provider>
-  , document.getElementById(anchor.id));
+render(<App/>, document.getElementById(anchor.id));
