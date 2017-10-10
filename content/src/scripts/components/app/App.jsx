d3db1ab7edcf03a22f4aee4a33eff16f536538d2
Edwin Morris <edwin@rapgenius.com>
2017-10-09 21:06:51 -0400

    Add download button that finds the url and downloads it

--- content/src/scripts/components/app/App.jsx
+++ content/src/scripts/components/app/App.jsx
@@ -7,24 +7,47 @@ class App extends Component {
   }
 
   componentDidMount() {
-    document.addEventListener('click', () => {
-      this.props.dispatch({
-        type: 'ADD_COUNT'
-      });
-    });
+    this.setDownloadImage();
+  }
+
+  pickSourceFromSrcset(srcset, filterByConstraint) {
+    return srcset.map((sourceAndConstraint) => {
+      const [source, constraint] = sourceAndConstraint.split(' ');
+
+      if (constraint === filterByConstraint) return source;
+    }).join('').trim();
+  }
+
+  findImage() {
+    const imageTag = Array.from(document.querySelectorAll('img')).find((image) => image.naturalWidth > 200);
+    const srcset = imageTag.srcset.split(',');
+
+    return this.pickSourceFromSrcset(srcset, '1080w');
+  }
+
+  setDownloadImage() {
+    const imageUrl = findImage();
+    this.downloadButton.href = imageUrl;
   }
 
   render() {
+    const buttonStyle = {
+      position: 'fixed',
+      bottom: '10vh',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      zIndex: '9999'
+    }
+
     return (
-      <div style={{
-        position: 'fixed',
-        bottom: '10vh',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        zIndex: '9999'
-      }}>
-        Count: {this.props.count}
-      </div>
+      <a
+        ref={(downloadButton) => { this.downloadButton = downloadButton; }}
+        style={buttonStyle}
+        href=""
+        download=""
+      >
+        Download High Resolution Copy
+      </a>
     );
   }
 }
