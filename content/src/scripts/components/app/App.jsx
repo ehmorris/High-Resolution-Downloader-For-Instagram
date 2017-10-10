d3d8766b3242cbf96fde09afeb6e4b6e28b380b9
Edwin Morris <edwin@rapgenius.com>
2017-10-09 22:04:24 -0400

    update copy

--- content/src/scripts/components/app/App.jsx
+++ content/src/scripts/components/app/App.jsx
@@ -55,7 +55,7 @@ class App extends Component {
         href=""
         download=""
       >
-        Download High Resolution Copy
+        Download Highest Resolution Copy
       </a>
     );
   }
