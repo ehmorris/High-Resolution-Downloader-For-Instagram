c205d8b839d417bcc17b77f1c9bad89d71e6504d
Edwin Morris <edwin@rapgenius.com>
2017-10-09 22:50:34 -0400

    generate buttons for each download option

--- content/src/scripts/components/app/App.jsx
+++ content/src/scripts/components/app/App.jsx
@@ -3,60 +3,104 @@ import React, {Component} from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.buttonHeight = '28px'
+    this.buttonContainerHeight = '28px'
   }
 
   componentDidMount() {
-    this.setDownloadImage();
-    document.body.style.transform = `translateY(${this.buttonHeight})`;
+    document.body.style.transform = `translateY(${this.buttonContainerHeight})`;
   }
+
+  findLargeAsset(tagName) {
+    const assets = document.querySelectorAll(tagName);
+    if (assets) {
+      return Array.from(assets).find((asset) => asset.clientWidth > 200);
+    } else {
+      return false;
+    }
+  }
+
+  findImage() { return this.findLargeAsset('img'); }
+
+  findVideo() { return this.findLargeAsset('video'); }
+
+  findVideoPosterUrl(video) { return video.poster; }
+
+  findVideoUrl(video) { return video.src; }
 
   pickSourceFromSrcset(srcset, filterByConstraint) {
     return srcset.map((sourceAndConstraint) => {
       const [source, constraint] = sourceAndConstraint.split(' ');
-
       if (constraint === filterByConstraint) return source;
     }).join('').trim();
   }
 
-  findImage() {
-    const imageTag = Array.from(document.querySelectorAll('img')).find((image) => image.naturalWidth > 200);
+  findImageUrl(imageTag) {
     const srcset = imageTag.srcset.split(',');
-
     return this.pickSourceFromSrcset(srcset, '1080w');
   }
 
-  setDownloadImage() {
-    const imageUrl = this.findImage();
-    this.downloadButton.href = imageUrl;
+  generateButton(link, copy = 'Download') {
+    return (
+      <a
+        style={{
+          color: '#fff',
+          margin: '0 .5em'
+        }}
+        key={link}
+        href={link}
+        download={link}
+      >{copy}</a>
+    )
+  }
+
+  renderButtons() {
+    const buttons = [];
+
+    if (this.findVideo()) {
+      const video = this.findVideo();
+
+      buttons.push(this.generateButton(
+        this.findVideoPosterUrl(video),
+        'Download video thumbnail'
+      ));
+
+      buttons.push(this.generateButton(
+        this.findVideoUrl(video),
+        'Download video'
+      ));
+    }
+
+    if (this.findImage()) {
+      buttons.push(this.generateButton(
+        this.findImageUrl(this.findImage()),
+        'Download image'
+      ));
+    }
+
+    return buttons;
   }
 
   render() {
-    const buttonStyles = {
+    const buttonContainerStyles = {
       background: '#3897f0',
       border: '1px solid #3897f0',
-      color: '#fff',
+      display: 'block',
       font: '600 14px/26px -apple-system, BlinkMacSystemFont, sans-serif',
-      height: this.buttonHeight,
+      height: this.buttonContainerHeight,
       left: '0',
       outline: 'none',
       padding: '0 8px',
       position: 'absolute',
       textAlign: 'center',
-      top: `-${this.buttonHeight}`,
+      top: `-${this.buttonContainerHeight}`,
       width: '100%',
       zIndex: '1'
     }
 
     return (
-      <a
-        ref={(downloadButton) => { this.downloadButton = downloadButton; }}
-        style={buttonStyles}
-        href=""
-        download=""
-      >
-        Download Highest Resolution Copy
-      </a>
+      <div style={buttonContainerStyles}>
+        {this.renderButtons()}
+      </div>
     );
   }
 }
