c8586349dd3f6ab85dfea564673a458b0a93c45d
Edwin Morris <edwin@rapgenius.com>
2017-10-09 21:58:14 -0400

    remove event code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,18 +5,7 @@ import rimraf from 'rimraf';
 
 const plugins = loadPlugins();
 
-import eventWebpackConfig from './event/webpack.config';
 import contentWebpackConfig from './content/webpack.config';
-
-gulp.task('event-js', ['clean'], (cb) => {
-  webpack(eventWebpackConfig, (err, stats) => {
-    if(err) throw new plugins.util.PluginError('webpack', err);
-
-    plugins.util.log('[webpack]', stats.toString());
-
-    cb();
-  });
-});
 
 gulp.task('content-js', ['clean'], (cb) => {
   webpack(contentWebpackConfig, (err, stats) => {
@@ -37,11 +26,10 @@ gulp.task('clean', (cb) => {
   rimraf('./build', cb);
 });
 
-gulp.task('build', ['copy-manifest', 'event-js', 'content-js']);
+gulp.task('build', ['copy-manifest', 'content-js']);
 
 gulp.task('watch', ['default'], () => {
   gulp.watch('content/**/*', ['build']);
-  gulp.watch('event/**/*', ['build']);
 });
 
 gulp.task('default', ['build']);
