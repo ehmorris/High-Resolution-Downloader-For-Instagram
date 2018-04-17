18d6cdbfffb97dabda4cfcf88d3354e4f1f53ad1
Edwin <edwin.h.morris@gmail.com>
2018-04-16 21:49:56 -0400

    Change extension name and fix trademark violations
    From PR (#20)
    
    Fixed bugs:
    
    - (#19) Extension has been removed from Chrome Web Store
    
    -----
    
    - Updates build

--- content/src/index.js
+++ content/src/index.js
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 import App from './App';
 
 const anchor = document.createElement('div');
-anchor.id = 'instagram-high-resolution-download-button';
+anchor.id = 'extension-button';
 
 document.body.insertBefore(anchor, document.body.childNodes[0]);
 
